cc2c04275f60b2db3436194bb71950e1123ca83a
Anuska <161431021+Nushhhhhhh@users.noreply.github.com>
2025-04-11 21:15:24 +0545

    Add files via upload

---
+++ Exercises/GettingStarted/src/week1/DiamondShape.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class DiamondShape {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("   *");
+	    System.out.println("  ***");
+	    System.out.println(" *****");
+	    System.out.println("*******");
+	    System.out.println(" *****");
+	    System.out.println("  ***");
+	    System.out.println("   *");
+	}
+}
---
+++ Exercises/GettingStarted/src/week1/PersonalDetails.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class PersonalDetails {
+
+	public static void main(String[] args) 
+	{
+		//personal details information
+		String Name= "Anuska";
+		String Address= "Patan, Lalitpur";
+		int Age=19;
+		String PhoneNumber= "9803377133";
+		//print details
+		System.out.println("Name: " + Name);
+		System.out.println("Address: " + Address);
+		System.out.println("Age: " + Age);
+		System.out.println("Phone number: " + PhoneNumber);
+	}
+}
